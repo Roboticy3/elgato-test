5a65f5bfafb2d45317f2534ef3198654541fc677
Roboticy3 <fribarz123@gmail.com>
2022-06-26 19:18:00 -0400

    Revised MoveCamera.cs

--- Assets/PlayerScripts/MoveCamera.cs
+++ Assets/PlayerScripts/MoveCamera.cs
@@ -1,25 +1,22 @@
 using UnityEngine;
 using Photon.Pun;
 
+/// <summary>
+/// Attach to the client's main camera to move and rotate it into the right position
+/// </summary>
 public class MoveCamera : MonoBehaviour {
 
-    public Transform player;
+    [Header("Player Reference")]
+    //reference to player
     public PlayerMovement pm;
-    private Quaternion desiredRotation;
-    private Quaternion currentrotation;
+
+    [Header("Tilt Options")]
+    //the amount the camera will tilt when wallrunning in degrees
+    public float tilt = 10f;
     
     void Update() {
-        transform.position = player.transform.position;
-        if (pm.WallRunning) {
-            if (pm.WallRunDirection == 1){
-                    transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, -10);
-            }
-            if (pm.WallRunDirection == -1){
-                    transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, 10);
-            }
-        }
-        else {
-            transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, 0);
-        }
+        transform.position = pm.playerCam.transform.position;
+        Vector3 eul = transform.localRotation.eulerAngles;
+        transform.localRotation = Quaternion.Euler(eul.x, eul.y, pm.WallRunDirection * -tilt);
     }
 }
